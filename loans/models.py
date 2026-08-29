dda7484ab34755bafe5d788041d6dcb6122ab6ae
AbdelazemAlaaEldin <abkarinoalaa@gmail.com>
2026-08-29 17:22:41 +0300

    Complete Day 2: Models and Authentication

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Book
+    #create your models here.
+class Loan(models.Model):
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(null=True, blank=True)
 
-# Create your models here.
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('borrowed', 'Borrowed'),
+            ('returned', 'Returned'),
+        ],
+        default='borrowed'
+    )
+
+    def __str__(self):
+        return f"{self.member.username} - {self.book.title}"
