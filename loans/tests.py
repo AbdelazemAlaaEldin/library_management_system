97d6863beb595958a0ec603490012859d81841c6
PikaAlaa <pikaalaa123@gmail.com>
2026-09-01 13:57:23 +0300

    Update borrowing and return workflow

--- loans/tests.py
+++ loans/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from catalog.models import Book
+from .models import Loan
 
-# Create your tests here.
+
+class BorrowingTests(TestCase):
+    def setUp(self):
+        self.member = User.objects.create_user('reader')
+        self.other = User.objects.create_user('anotherreader')
+        self.book = Book.objects.create(title='A library book', author='An author',
+            isbn='1234567890123', category='Fiction', total_copies=1,
+            available_copies=1, publication_date='2020-01-01')
+        self.client.force_login(self.member)
+
+    def test_borrow_and_return_change_inventory_exactly_once(self):
+        url = f'/borrow/{self.book.pk}/'
+        self.assertEqual(self.client.get(url).status_code, 405)
+        self.client.post(url)
+        self.client.post(url)
+        self.book.refresh_from_db()
+        self.assertEqual(self.book.available_copies, 0)
+        self.assertEqual(Loan.objects.filter(status='borrowed').count(), 1)
+        loan = Loan.objects.get()
+        self.assertContains(self.client.get('/?section=loans'), 'A library book')
+        self.assertEqual(self.client.get(f'/return/{loan.pk}/').status_code, 405)
+        self.client.post(f'/return/{loan.pk}/')
+        self.client.post(f'/return/{loan.pk}/')
+        self.book.refresh_from_db()
+        loan.refresh_from_db()
+        self.assertEqual(self.book.available_copies, 1)
+        self.assertEqual(loan.status, 'returned')
+        self.assertIsNotNone(loan.return_date)
+
+    def test_cannot_borrow_unavailable_book(self):
+        self.book.available_copies = 0
+        self.book.save()
+        self.client.post(f'/borrow/{self.book.pk}/')
+        self.assertFalse(Loan.objects.exists())
+
+    def test_other_member_cannot_view_or_return_private_loan(self):
+        self.client.post(f'/borrow/{self.book.pk}/')
+        loan = Loan.objects.get()
+        self.client.force_login(self.other)
+        self.assertNotContains(self.client.get('/?section=loans'), 'Borrowed Jan')
+        self.assertEqual(len(self.client.get('/?section=loans').context['loans']), 0)
+        self.assertEqual(self.client.post(f'/return/{loan.pk}/').status_code, 404)
+        loan.refresh_from_db()
+        self.assertEqual(loan.status, 'borrowed')
+
+    def test_csrf_required_for_borrow(self):
+        from django.test import Client
+        client = Client(enforce_csrf_checks=True)
+        client.force_login(self.member)
+        self.assertEqual(client.post(f'/borrow/{self.book.pk}/').status_code, 403)
+        self.assertFalse(Loan.objects.exists())
--- loans/views.py
+++ loans/views.py
@@ -1,62 +1,36 @@
-from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.db.models import F
 from django.utils import timezone
+from django.views.decorators.http import require_POST
 from .models import Loan
 from catalog.models import Book
 
-# Create your views here.
+
 @login_required
+@require_POST
+@transaction.atomic
 def borrow_book(request, book_id):
-
     book = get_object_or_404(Book, id=book_id)
-
-    # Check if there are available copies
-    if book.available_copies <= 0:
-        return redirect('home')
-
-    # Check if the user already borrowed this book
-    existing_loan = Loan.objects.filter(
-        member=request.user,
-        book=book,
-        status='borrowed'
-    ).exists()
-
-    if existing_loan:
-        return redirect('home')
-
-    # Create new loan
-    Loan.objects.create(
-        member=request.user,
-        book=book,
-        status='borrowed'
-    )
-
-    # Decrease available copies
-    book.available_copies -= 1
-    book.save()
-
+    if Loan.objects.filter(member=request.user, book=book, status='borrowed').exists():
+        messages.info(request, 'This book is already on your borrowing shelf.')
+    elif Book.objects.filter(pk=book.pk, available_copies__gt=0).update(available_copies=F('available_copies') - 1):
+        Loan.objects.create(member=request.user, book=book)
+        messages.success(request, f'“{book.title}” is now on your borrowing shelf. Happy reading!')
+    else:
+        messages.warning(request, 'All copies are currently borrowed. Please check back soon.')
     return redirect('home')
 
 
 @login_required
+@require_POST
+@transaction.atomic
 def return_book(request, loan_id):
-
-    loan = get_object_or_404(
-        Loan,
-        id=loan_id,
-        member=request.user,
-        status='borrowed'
-    )
-
-    # Change loan status
-    loan.status = 'returned'
-    loan.return_date = timezone.now().date()
-    loan.save()
-
-    # Increase available copies
-    book = loan.book
-    book.available_copies += 1
-    book.save()
-
-    return redirect('home')
+    loan = get_object_or_404(Loan, id=loan_id, member=request.user)
+    updated = Loan.objects.filter(pk=loan.pk, status='borrowed').update(status='returned', return_date=timezone.localdate())
+    if updated:
+        Book.objects.filter(pk=loan.book_id).update(available_copies=F('available_copies') + 1)
+        messages.success(request, 'Book returned. There is always another story waiting.')
+    return redirect('/?section=loans')
